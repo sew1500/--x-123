7a2b81e1da57e6b5329d9b883db57f9a28e120da
𝙿𝚁𝙸𝙼𝙴 𝙰𝙻𝙿𝙷𝙰-𝙼𝙳 𝚅 01.0 <primealpha727@gmail.com>
2025-08-04 12:33:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IK~1Pc0WSJB#8_e_zhkk44GxQWHTERq9MDc5rAxQrYDHYVbcknrcF5w",
+SESSION_ID: process.env.SESSION_ID || "&GspwhCIC#CMzgRmwdWr5tBC7dmzmGUCcjF_mxSDuGr5Owb-WHdAI",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
